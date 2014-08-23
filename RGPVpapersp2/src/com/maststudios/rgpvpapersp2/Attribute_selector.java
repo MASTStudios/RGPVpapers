6482eb90f9ee9c1ca088d103b9e666bcca5411aa
Tanay <tanaysoley@gmail.com>
2014-08-23 11:10:42 +0530

    adding android project default files

---
+++ RGPVpapersp2/src/com/maststudios/rgpvpapersp2/Attribute_selector.java
@@ -0,0 +1,22 @@
+package com.maststudios.rgpvpapersp2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Attribute_selector extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_attribute_selector);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.attribute_selector, menu);
+		return true;
+	}
+
+}
