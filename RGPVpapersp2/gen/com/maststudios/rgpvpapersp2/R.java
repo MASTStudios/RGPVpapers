8e5c3a1cadeaa16034c68a584f010f9df4d4788e
Tanay <tanaysoley@gmail.com>
2014-09-06 15:54:59 +0530

    minor changes

--- RGPVpapersp2/gen/com/maststudios/rgpvpapersp2/R.java
+++ RGPVpapersp2/gen/com/maststudios/rgpvpapersp2/R.java
@@ -10,6 +10,11 @@ package com.maststudios.rgpvpapersp2;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int dark_blue=0x7f040000;
+        public static final int light_gray=0x7f040002;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +22,33 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int list_item_padding=0x7f050002;
+        public static final int year_block_side=0x7f050003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int RelativeLayout1=0x7f090000;
+        public static final int action_settings=0x7f090005;
+        public static final int imageView1=0x7f090001;
+        public static final int textView1=0x7f090003;
+        public static final int textView2=0x7f090004;
+        public static final int year=0x7f090002;
     }
     public static final class layout {
         public static final int activity_attribute_selector=0x7f030000;
+        public static final int list_item_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int attribute_selector=0x7f070000;
+        public static final int attribute_selector=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
